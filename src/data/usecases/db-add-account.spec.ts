cca97575b1566015b77491a5fd68c51c347473c8
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-15 16:02:35 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/db-add-account.spec.ts
+++ src/data/usecases/db-add-account.spec.ts
@@ -2,29 +2,43 @@ import { AccountReturnedByDbModel } from '../../domain/models/account-returned-b
 import { LoadAccountByEmailRepository } from '../protocols/db/load-account-by-email-repository'
 import { DbAddAccount } from './db-add-account'
 
+type SutTypes = {
+  sut: DbAddAccount
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
+}
+
+const makeSut = (): SutTypes => {
+  class LoadAccountByEmailRepositoryStub
+    implements LoadAccountByEmailRepository {
+    async loadByEmail(email: string): Promise<AccountReturnedByDbModel> {
+      const fakeAccount = {
+        id: 'any_id',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+
+      return await new Promise((resolve) => resolve(fakeAccount))
+    }
+  }
+
+  const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
+
+  const sut = new DbAddAccount(loadAccountByEmailRepositoryStub)
+
+  return {
+    sut,
+    loadAccountByEmailRepositoryStub
+  }
+}
+
 describe('DbAddAccount Usecase', () => {
   test('Should call LoadAccountByEmailRepository with correct email', async () => {
-    class LoadAccountByEmailRepositoryStub
-      implements LoadAccountByEmailRepository {
-      async loadByEmail(email: string): Promise<AccountReturnedByDbModel> {
-        const fakeAccount = {
-          id: 'any_id',
-          email: 'any_email@mail.com',
-          password: 'any_password'
-        }
-
-        return await new Promise((resolve) => resolve(fakeAccount))
-      }
-    }
-
-    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
 
     const loadByEmailSpy = jest.spyOn(
       loadAccountByEmailRepositoryStub,
       'loadByEmail'
     )
-
-    const sut = new DbAddAccount(loadAccountByEmailRepositoryStub)
 
     await sut.add({
       email: 'any_email@mail.com',
