2c02b93ba281facdfce263513f699c7f6ab3cd47
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-15 16:50:53 -0300

    feat: ensure DbAddAccount calls Hasher with correct password

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,14 +1,16 @@
 import { AccountReturnedByDbModel } from '../../domain/models/account-returned-by-db-model'
 import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account'
+import { Hasher } from '../protocols/criptography/hasher'
 import { LoadAccountByEmailRepository } from '../protocols/db/load-account-by-email-repository'
 
 export class DbAddAccount implements AddAccount {
   constructor(
-    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hasher: Hasher
   ) {}
 
   async add(accountData: AddAccountModel): Promise<AccountReturnedByDbModel> {
-    const { email } = accountData
+    const { email, password } = accountData
 
     const isEmailAlreadyInUse = await this.loadAccountByEmailRepository.loadByEmail(
       email
@@ -17,5 +19,7 @@ export class DbAddAccount implements AddAccount {
     if (isEmailAlreadyInUse) {
       return null
     }
+
+    await this.hasher.hash(password)
   }
 }
