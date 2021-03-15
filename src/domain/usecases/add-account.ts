ad8f44af667811503319a835a1632cb71dbbb5eb
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-15 16:14:52 -0300

    refactor: make AddAccountModel be a Omit of AccountReturnedByDbModel, without the id

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,9 +1,6 @@
 import { AccountReturnedByDbModel } from '../models/account-returned-by-db-model'
 
-export interface AddAccountModel {
-  email: string
-  password: string
-}
+export type AddAccountModel = Omit<AccountReturnedByDbModel, 'id'>
 
 export interface AddAccount {
   add: (data: AddAccountModel) => Promise<AccountReturnedByDbModel>
