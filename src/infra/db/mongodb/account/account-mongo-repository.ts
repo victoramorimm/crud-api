b7f3036f143de0b295af7ac3dad0ce5aa2fcf5ed
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-16 11:29:58 -0300

    feat: ensure AccountMongoRepository returns an account on add success

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,8 +1,10 @@
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { AccountReturnedByDbModel } from '../../../../data/models/account-returned-by-db-model'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { AddAccountRepository } from '../../../../data/usecases/db-add-account-protocols'
 
-export class AccountMongoRepository implements LoadAccountByEmailRepository {
+export class AccountMongoRepository
+  implements LoadAccountByEmailRepository, AddAccountRepository {
   async loadByEmail(email: string): Promise<AccountReturnedByDbModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
 
@@ -17,5 +19,19 @@ export class AccountMongoRepository implements LoadAccountByEmailRepository {
     }
 
     return null
+  }
+
+  async add(accountData: any): Promise<AccountReturnedByDbModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+
+    const result = await accountCollection.insertOne(accountData)
+
+    const accountReturnedByDb = result.ops[0]
+
+    const account = await MongoHelper.makeAdapterForTheAccountIdReturnedByDb(
+      accountReturnedByDb
+    )
+
+    return account
   }
 }
