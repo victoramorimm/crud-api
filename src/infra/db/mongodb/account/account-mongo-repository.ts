f52c9a4dc1c1d2d335c128d9868ef50e345943d9
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-16 11:35:35 -0300

    refactor: make add method on AccountMongoRepository receive an AddAccountModel

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -2,6 +2,7 @@ import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load
 import { AccountReturnedByDbModel } from '../../../../data/models/account-returned-by-db-model'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AddAccountRepository } from '../../../../data/usecases/db-add-account-protocols'
+import { AddAccountModel } from '../../../../data/models/add-account-model'
 
 export class AccountMongoRepository
   implements LoadAccountByEmailRepository, AddAccountRepository {
@@ -21,7 +22,7 @@ export class AccountMongoRepository
     return null
   }
 
-  async add(accountData: any): Promise<AccountReturnedByDbModel> {
+  async add(accountData: AddAccountModel): Promise<AccountReturnedByDbModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
 
     const result = await accountCollection.insertOne(accountData)
