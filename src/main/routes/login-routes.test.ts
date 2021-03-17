689d2e5bf1d1d2fb98c62720b052ace1c50704ad
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-17 15:16:54 -0300

    test: ensure LoginRoutes returns 401 on login

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -37,4 +37,14 @@ describe('Login Routes', () => {
       })
       .expect(200)
   })
+
+  test('Should return 401 on login', async () => {
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'victoramorim@gmail.com',
+        password: '123456'
+      })
+      .expect(401)
+  })
 })
