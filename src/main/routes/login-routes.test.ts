e6d5b6321a19d2e12af5bb4e38d95a512b2c40ca
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-17 15:14:39 -0300

    test: ensure LoginRoutes returns 200 on login

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,8 +1,12 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 
-describe('SignUp Routes', () => {
+let accountCollection: Collection
+
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -12,18 +16,24 @@ describe('SignUp Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
 
     await accountCollection.deleteMany({})
   })
 
-  test('Should return 200 on signup', async () => {
+  test('Should return 200 on login', async () => {
+    const password = await hash('123456', 12)
+
+    await accountCollection.insertOne({
+      email: 'victoramorim@gmail.com',
+      password
+    })
+
     await request(app)
-      .post('/api/signup')
+      .post('/api/login')
       .send({
         email: 'victoramorim@gmail.com',
-        password: '123456',
-        passwordConfirmation: '123456'
+        password: '123456'
       })
       .expect(200)
   })
