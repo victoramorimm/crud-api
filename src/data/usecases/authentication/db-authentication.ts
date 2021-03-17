e164fbf51a871de0f6ce2f895adf4bd462bb6243
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-17 10:47:10 -0300

    feat: ensure DbAuthentication calls Encrypter with correct id

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,13 +2,15 @@ import {
   Authentication,
   AuthenticationModel
 } from '../../../domain/usecases/authentication'
+import { Encrypter } from '../../protocols/criptography/encrypter'
 import { HashComparer } from '../../protocols/criptography/hash-comparer'
 import { LoadAccountByEmailRepository } from '../signup/db-add-account-protocols'
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter
   ) {}
 
   async auth(data: AuthenticationModel): Promise<string> {
@@ -24,6 +26,8 @@ export class DbAuthentication implements Authentication {
       value: password,
       valueToCompare: account.password
     })
+
+    await this.encrypter.encrypt(account.id)
 
     return null
   }
