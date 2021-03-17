34b537df0515cdfb451dbbcdb92287d749c4e065
Victor Amorim <victorvmrgamer@gmail.com>
2021-03-17 13:55:05 -0300

    test: ensure JwtAdapter returns an accessToken on sign success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  async sign(): Promise<string> {
+    return 'any_token'
+  }
+}))
+
 describe('Jwt Adapter', () => {
   describe('sign()', () => {
     test('Should call sign with correct values', async () => {
@@ -13,6 +19,16 @@ describe('Jwt Adapter', () => {
       await sut.encrypt('any_value')
 
       expect(signSpy).toHaveBeenCalledWith({ value: 'any_value' }, secret)
+    })
+
+    test('Should return an accessToken on sign success', async () => {
+      const secret = 'any_secret'
+
+      const sut = new JwtAdapter(secret)
+
+      const accessToken = await sut.encrypt('any_value')
+
+      expect(accessToken).toBe('any_token')
     })
   })
 })
